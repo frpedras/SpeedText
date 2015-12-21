1ae2a48b7e3216bb681299bef0491c2c8bfa9d8f
frpedras <frpedras@gmail.com>
2015-12-21 04:22:48 +0000

    Javadoc added to SpeedTextSortList

--- pa.iscde.speedtext/src/pa/iscde/speedtext/SpeedTextSortList.java
+++ pa.iscde.speedtext/src/pa/iscde/speedtext/SpeedTextSortList.java
@@ -3,6 +3,14 @@ package pa.iscde.speedtext;
 
 import java.util.Collection;
 
+/**
+ * Represents an extension point to sort the sugestion list
+ */
 public interface SpeedTextSortList {
+	/**
+	 * Receives the sugestion list and returns it sorted, with the same elements
+	 * @param list Collection of strings to sort
+	 * @return a Collection sorted
+	 */
 	public Collection<String> sortList(Collection<String> list);
 }
