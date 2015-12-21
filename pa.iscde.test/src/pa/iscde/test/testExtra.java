6cff362eded8eddd298e9a1de5e803c3cdd31748
n0gas <jorgemmnogueira@hotmail.com>
2015-12-21 16:42:10 +0000

    file

--- pa.iscde.test/src/pa/iscde/test/testExtra.java
+++ pa.iscde.test/src/pa/iscde/test/testExtra.java
@@ -1,18 +1,23 @@
 package pa.iscde.test;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Collection;
+
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.VariableDeclarationStatement;
 
 import pa.iscde.speedtext.SpeedTextExtraInfo;
 
 public class testExtra implements SpeedTextExtraInfo {
 
 	@Override
-	public Collection<String> extraInfo(Collection<String> list) {
+	public Collection<String> extraInfo(Collection<String> list,File file) {
 		System.out.println("antes: "+list.toString());
 		Object[] aux = list.toArray();
 		list.clear();
 		for(int i=0; i<aux.length;i++){
-			list.add(aux[i]+" - "+"banana");
+			list.add("info"+i);
 		}
 		System.out.println("depois: "+list.toString());	
 		return list;
